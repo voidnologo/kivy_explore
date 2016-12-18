cf178cd59119716485eddc49531765329ba33ab9
voidnologo <(none)>
2016-12-18 12:19:37 -0600

    check score; alpha

--- letters/letters.py
+++ letters/letters.py
@@ -3,7 +3,7 @@ import random
 import time
 
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ListProperty
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
@@ -13,6 +13,7 @@ from kivy.core.window import Window
 class LettersWidget(Widget):
     letter = StringProperty()
     score = StringProperty()
+    score_color = ListProperty([1, 1, 1, 1])
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -20,19 +21,29 @@ class LettersWidget(Widget):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self.get_letter()
 
-    def get_letter(self):
+    def get_letter(self, *args):
+        self.ids.letter.color = self.random_color
         self.letter = random.choice(string.ascii_uppercase)
 
     def check_input(self, key):
+        self.set_letter_alpha(0.4)
         if key.upper() == self.letter:
             self.score = 'OK'
-            self.get_letter()
+            self.score_color = self.ok_color
+            Clock.schedule_once(self.get_letter, 1)
         else:
             self.score = 'WRONG'
+            self.score_color = self.wrong_color
         Clock.schedule_once(self.clear_score, 1)
 
-    def clear_score(self, dt):
+    def set_letter_alpha(self, alpha):
+        letter_color = self.ids.letter.color
+        letter_color[3] = alpha
+        self.ids.letter.color = letter_color
+
+    def clear_score(self, *args):
         self.score = ''
+        self.set_letter_alpha(1)
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
@@ -40,6 +51,22 @@ class LettersWidget(Widget):
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         self.check_input(keycode[1])
+
+    @property
+    def random_color(self):
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        return (r, g, b, 1)
+
+    @property
+    def ok_color(self):
+        return (0, 1, 0, 1)
+
+    @property
+    def wrong_color(self):
+        return (1, 0, 0, 1)
+
 
 
 class LettersApp(App):
